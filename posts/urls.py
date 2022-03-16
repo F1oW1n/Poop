c12f0ea4b258546455f9d149c72b6182f2d7f955
Your Name <you@example.com>
2022-03-16 20:18:30 +0600

    nbc

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,20 @@
+
 from django.urls import path
 from . import views
 from .views import (
     PostListview,
-
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
 
 )
 
 urlpatterns = [
     path('', PostListview.as_view(), name='home'),
+    path('post/new/', PostCreateView.as_view(), name='create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('about/', views.about, name='about')
 ]
